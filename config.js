c8e186dfa4f8eae052a0b8a4ef147a86eb86c0cb
eco64 <63120383+eco64@users.noreply.github.com>
2020-04-03 20:21:50 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,74 @@
+var CONFIG = {
+  "maps": {
+    "World": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1585877655,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 9,
+      "name": "World",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "world_isometric_t1",
+      "tileSize": 384,
+      "world": "world",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    },
+    "World_Cave": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1585877655,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 9,
+      "name": "World - Cave",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "world_isometric_t1",
+      "tileSize": 384,
+      "world": "world",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "World",
+    "World_Cave"
+  ],
+  "tileSetGroups": {
+    "world_isometric_t1": {
+      "maxZoom": 9,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
